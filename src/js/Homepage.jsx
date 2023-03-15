56bf25935f20eca7fb518a47c67dffd6e50ebaf0
Mathis-COCO <mathis.coco@ynov.com>
2023-03-15 01:42:29 +0100

    homepage searchbar, background, title

--- src/js/Homepage.jsx
+++ src/js/Homepage.jsx
@@ -1,11 +1,18 @@
 /* eslint-disable indent */
 import React from 'react';
 import '../css/App.scss';
+import {FiSearch} from 'react-icons/fi';
 
 function Homepage() {
   return (
-    <div>
+    <div className='main-div'>
         <h1 className='home-title'>HOMEPAGE</h1>
+        <div>
+            <div className='searchbar'>
+                <input type='text' placeholder='Rechercher...'/>
+                <div><FiSearch color='white' fontSize='1.5em' /></div>
+            </div>
+        </div>
     </div>
   );
 }
